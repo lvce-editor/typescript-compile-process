f26bf8af89074744174199f2731074e4879765c5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-06-01 15:07:40 +0200

    feature: remove unused code (#10)

--- src/parts/PlatformPaths/PlatformPaths.ts
+++
@@ -1,8 +0,0 @@
-import * as Path from '../Path/Path.ts'
-import * as Root from '../Root/Root.ts'
-
-export const getBuiltinExtensionsPath = () => {
-  return (
-    process.env.BUILTIN_EXTENSIONS_PATH || Path.join(Root.root, 'extensions')
-  )
-}
