bc963f32a48fc74f704d3797d20b3dd0e9b838fd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-06-01 15:14:11 +0200

    feature: format code (#13)

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -3,9 +3,7 @@ import * as TranspileTypeScript from '../TranspileTypeScript/TranspileTypeScript
 import * as TypeScriptPath from '../TypeScriptPath/TypeScriptPath.ts'
 
 export const commandMap = {
-  'TranspileTypeScript.transpileTypeScript':
-    TranspileTypeScript.transpileTypeScript,
+  'TranspileTypeScript.transpileTypeScript': TranspileTypeScript.transpileTypeScript,
   'TypeScript.setTypeScriptPath': TypeScriptPath.set,
-  'HandleElectronMessagePort.handleElectronMessagePort':
-    HandleElectronMessagePort.handleElectronMessagePort,
+  'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
 }
