6f4abbf8e3bee3765597044dd34b188dc79bf24f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-10-28 18:40:13 +0100

    feature: use command helper package (#25)

--- src/parts/Command/Command.ts
+++ src/parts/Command/Command.ts
@@ -1,9 +1,1 @@
-import * as CommandState from '../CommandState/CommandState.ts'
-
-export const execute = (command, ...args) => {
-  const fn = CommandState.getCommand(command)
-  if (!fn) {
-    throw new Error(`Command not found ${command}`)
-  }
-  return fn(...args)
-}
+export * from '@lvce-editor/command'
--- src/parts/Main/Main.ts
+++ src/parts/Main/Main.ts
@@ -1,8 +1,8 @@
+import * as Command from '@lvce-editor/command'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import * as CommandState from '../CommandState/CommandState.ts'
 import * as Listen from '../Listen/Listen.ts'
 
 export const main = async () => {
-  CommandState.registerCommands(CommandMap.commandMap)
+  Command.register(CommandMap.commandMap)
   await Listen.listen()
 }
