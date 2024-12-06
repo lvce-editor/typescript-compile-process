cfe00912731b3ee3a4640ea6d2b74d965568477c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-06 22:52:20 +0100

    feature: format code (#36)

--- src/parts/TranspileTypeScriptAtPath/TranspileTypeScriptAtPath.ts
+++ src/parts/TranspileTypeScriptAtPath/TranspileTypeScriptAtPath.ts
@@ -1,11 +1,11 @@
 import { VError } from '@lvce-editor/verror'
 import * as TranspileTypeScript from '../TranspileTypeScript/TranspileTypeScript.ts'
-import {readFile, writeFile} from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises'
 
-export const transpileTypeScriptAtPath = async (inPath:string, outPath:string):Promise<void> => {
+export const transpileTypeScriptAtPath = async (inPath: string, outPath: string): Promise<void> => {
   try {
-    const content=await readFile(inPath, 'utf-8')
-    const newContent=await TranspileTypeScript.transpileTypeScript(content)
+    const content = await readFile(inPath, 'utf-8')
+    const newContent = await TranspileTypeScript.transpileTypeScript(content)
     await writeFile(outPath, newContent)
   } catch (error) {
     throw new VError(error, `Failed to transpile typescript`)
