0be04caad159254b3fabdef67554b67b3879568a
Le Vivilet <levivlet@pm.me>
2024-12-06 22:46:51 +0100

    feature: add transpileTypescriptAtPath command

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,9 +1,11 @@
 import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleElectronMessagePort.ts'
 import * as TranspileTypeScript from '../TranspileTypeScript/TranspileTypeScript.ts'
+import * as TranspileTypeScriptAtPath from '../TranspileTypeScriptAtPath/TranspileTypeScriptAtPath.ts'
 import * as TypeScriptPath from '../TypeScriptPath/TypeScriptPath.ts'
 
 export const commandMap = {
   'TranspileTypeScript.transpileTypeScript': TranspileTypeScript.transpileTypeScript,
+  'TranspileTypeScript.transpileTypeScriptAtPath': TranspileTypeScriptAtPath.transpileTypeScriptAtPath,
   'TypeScript.setTypeScriptPath': TypeScriptPath.set,
   'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
 }
---
+++ src/parts/TranspileTypeScriptAtPath/TranspileTypeScriptAtPath.ts
@@ -0,0 +1,13 @@
+import { VError } from '@lvce-editor/verror'
+import * as TranspileTypeScript from '../TranspileTypeScript/TranspileTypeScript.ts'
+import {readFile, writeFile} from 'node:fs/promises'
+
+export const transpileTypeScriptAtPath = async (inPath:string, outPath:string):Promise<void> => {
+  try {
+    const content=await readFile(inPath, 'utf-8')
+    const newContent=await TranspileTypeScript.transpileTypeScript(content)
+    await writeFile(outPath, newContent)
+  } catch (error) {
+    throw new VError(error, `Failed to transpile typescript`)
+  }
+}
