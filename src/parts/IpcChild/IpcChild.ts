5640fdc5a3d77d16f4e799068a17897cf3118c3d
Le Vivilet <levivlet@pm.me>
2025-01-17 17:26:41 +0100

    types

--- src/parts/IpcChild/IpcChild.ts
+++ src/parts/IpcChild/IpcChild.ts
@@ -1,6 +1,6 @@
 import * as IpcChildModule from '../IpcChildModule/IpcChildModule.ts'
 
-export const listen = async ({ method, ...params }: { readonly method: number; readonly params: any }) => {
+export const listen = async ({ method, ...params }: { readonly method: number; readonly [key: string]: any }) => {
   const module = IpcChildModule.getModule(method)
   // @ts-ignore
   const rpc = await module.create(params)
