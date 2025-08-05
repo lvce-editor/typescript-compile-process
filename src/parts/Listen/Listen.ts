69c03e994aef84f744434d6006cc4cf7862910c4
Le Vivilet <levivlet@pm.me>
2025-08-05 16:55:38 +0200

    feature: remove unused dependencies

--- src/parts/Listen/Listen.ts
+++ src/parts/Listen/Listen.ts
@@ -1,6 +1,10 @@
 import * as IpcChild from '../IpcChild/IpcChild.ts'
 import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
+import * as CommandMap from '../CommandMap/CommandMap.ts'
 
 export const listen = async () => {
-  await IpcChild.listen({ method: IpcChildType.Auto() })
+  await IpcChild.listen({
+    commandMap: CommandMap.commandMap,
+    method: IpcChildType.Auto(),
+  })
 }
--- src/parts/Main/Main.ts
+++ src/parts/Main/Main.ts
@@ -3,6 +3,5 @@ import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as Listen from '../Listen/Listen.ts'
 
 export const main = async () => {
-  Command.register(CommandMap.commandMap)
   await Listen.listen()
 }
