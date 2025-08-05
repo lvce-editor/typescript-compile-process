5b3c90c4a914713926f4ac9de2372d5642e142bb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-05 17:00:01 +0200

    feature: remove unused dependencies (#79)
    * feature: remove unused dependencies
    
    * fix
    
    * gitignore
    
    * lint
    
    * tsc
    
    * tsc

--- src/parts/Listen/Listen.ts
+++ src/parts/Listen/Listen.ts
@@ -1,6 +1,10 @@
+import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as IpcChild from '../IpcChild/IpcChild.ts'
 import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
 
 export const listen = async () => {
-  await IpcChild.listen({ method: IpcChildType.Auto() })
+  await IpcChild.listen({
+    commandMap: CommandMap.commandMap,
+    method: IpcChildType.Auto(),
+  })
 }
--- src/parts/Main/Main.ts
+++ src/parts/Main/Main.ts
@@ -1,8 +1,5 @@
-import * as Command from '@lvce-editor/command'
-import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as Listen from '../Listen/Listen.ts'
 
 export const main = async () => {
-  Command.register(CommandMap.commandMap)
   await Listen.listen()
 }
