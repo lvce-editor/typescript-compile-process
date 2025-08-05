3c3173f9fb58355775fdce13e8ebc1c0983d4225
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-08-05 18:29:18 +0200

    feature: dynamically import striptypescriptypes to avoid potential breaking changes (#84)

--- packages/typescript-compile-process/src/parts/TranspileTypeScript2/TranspileTypeScript2.ts
+++ packages/typescript-compile-process/src/parts/TranspileTypeScript2/TranspileTypeScript2.ts
@@ -1,8 +1,8 @@
 import { VError } from '@lvce-editor/verror'
-import { stripTypeScriptTypes } from 'node:module'
 
 export const transpileTypeScript2 = async (code: string) => {
   try {
+    const { stripTypeScriptTypes } = await import('node:module')
     const transformed = stripTypeScriptTypes(code)
     return transformed
   } catch (error) {
