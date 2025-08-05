30e55738cf6d310903468feea4fb61ba37c5954d
Le Vivilet <levivlet@pm.me>
2025-08-05 16:58:20 +0200

    tsc

--- src/parts/Main/Main.ts
+++ src/parts/Main/Main.ts
@@ -1,5 +1,3 @@
-import * as Command from '@lvce-editor/command'
-import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as Listen from '../Listen/Listen.ts'
 
 export const main = async () => {
