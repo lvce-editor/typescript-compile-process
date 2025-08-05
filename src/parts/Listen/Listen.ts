1efb2fc968c2d730e3899a39f972db67e7a45035
Le Vivilet <levivlet@pm.me>
2025-08-05 16:57:30 +0200

    lint

--- src/parts/Listen/Listen.ts
+++ src/parts/Listen/Listen.ts
@@ -1,6 +1,6 @@
+import * as CommandMap from '../CommandMap/CommandMap.ts'
 import * as IpcChild from '../IpcChild/IpcChild.ts'
 import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
-import * as CommandMap from '../CommandMap/CommandMap.ts'
 
 export const listen = async () => {
   await IpcChild.listen({
