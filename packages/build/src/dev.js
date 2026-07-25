d5426c629d4f81c7e0fbca01669ae60d416985cf
Le Vivilet <levivilet@pm.me>
2026-07-25 22:24:10 +0000

    feature: replace lerna with npm workspaces

--- packages/build/src/dev.js
+++ packages/build/src/dev.js
@@ -6,7 +6,7 @@ const main = async () => {
     cwd: root,
     stdio: 'inherit',
   })
-  execa('node', ['packages/server/node_modules/@lvce-editor/server/bin/server.js', '--test-path=packages/e2e'], {
+  execa('node', ['node_modules/@lvce-editor/server/bin/server.js', '--test-path=packages/e2e'], {
     cwd: root,
     stdio: 'inherit',
   })
--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,14 +16,7 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x @types/node -x jest -x @jest/globals -x lerna -x typescript`
-  SUB='All dependencies match the latest package versions'
-  if [[ "$OUTPUT" == *"$SUB"* ]]; then
-    echo "$OUTPUT"
-  else
-    rm -rf node_modules package-lock.json dist
-    npm install
-  fi
+  ncu -u -x @types/node -x jest -x @jest/globals -x typescript
 }
 
                                                        updateDependencies             &&
@@ -32,6 +25,8 @@ cd packages/e2e                                     && updateDependencies && cd 
 cd packages/memory                                  && updateDependencies && cd ../.. &&
 cd packages/server                                  && updateDependencies && cd ../.. &&
 cd packages/typescript-compile-process              && updateDependencies && cd ../.. &&
+
+npm install
 
 echo "Great Success!"
 
